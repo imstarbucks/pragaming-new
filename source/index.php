f6b26411788d6bc6999d1e08cc3f100e3cc970e0
imstarbucks <spenwong98@gmail.com>
2022-01-09 01:12:05 +0800

    Hero font

--- source/index.php
+++ source/index.php
@@ -10,13 +10,19 @@ include 'head.php';
 		include 'include/navigation.php'
 	?>
 	<main>
-		<section class="home">
-			<h1 class="font-3xl">
-				Welcome To
-			</h1>
-			<h1>
-				Pragaming
-			</h1>
+		<section class="home position-relative">
+			<div class="d-flex flex-column align-items-center justify-content-center color-smoke absolute-center">
+				<h2 class="font-3xl">
+					Welcome To
+				</h2>
+				<h1 class="hero-logo font-header glitch color-soft-peach position-relative">
+					<span aria-hidden="true">Pragaming</span>Pragaming<span aria-hidden="true">Pragaming</span>
+				</h1>
+			</div>
+			<div class="scroll-indicator absolute-bottom">
+				<?= file_get_contents('img/mouse.svg'); ?>
+				<p>Scroll for more</p>
+			</div>
 		</section>
 	</main>
 </body>

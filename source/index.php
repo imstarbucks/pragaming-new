37f06743e06bc82eb2496488fe56471d02f6e801
imstarbucks <spenwong98@gmail.com>
2022-01-08 17:52:22 +0800

    init base style

--- source/index.php
+++ source/index.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Crying Neko Quick Start Web Dev</title>
-	<link rel="stylesheet" href="./main.css">
-</head>
+
+<?php
+include 'head.php';
+?>
+
 <body>
-	<h1 style="text-align: center;">A quick start Template for local web development</h1>
-	<img src='./img/crying-neko.jpeg' alt='Crying Cat' style="margin: auto; display: block;">
+	<main>
+		<h1>
+			Pragaming
+		</h1>
+		<p>
+			pragaming
+		</p>
+	</main>
 </body>
-<script src="./js/app.js"></script>
+
+<?php
+include 'script.php';
+?>
+
 </html>
